099e0dcc6f545ea4c736b0ae46f4d3d211b8aa32
anupparikh <anupparikh@1447cd30-8082-11de-88cc-eb9610995869>
2009-08-12 03:17:17 +0000

    convert data frame to example table now correctly handles missing values. rpy now looks for RPATH environment variable to set R home dir.

--- OrangeWidgets/R/OWRpy.py
+++ OrangeWidgets/R/OWRpy.py
@@ -6,40 +6,44 @@
 
 from OWWidget import *
 from rpy_options import set_options
-set_options(RHOME='I:/Program Files/R/R-2.7.0/')
+set_options(RHOME=os.environ['RPATH'])
 from rpy import *
 
 class OWRpy():
-	#a class variable which is incremented every time OWRpy is instantiated.
-	
-	num_widgets = 0
-	def __init__(self):	
-		#The class variable is used to create the unique names in R
-		OWRpy.num_widgets += 1
-		#this should be appended to every R variable
-		self.variable_suffix = '_' + str(OWRpy.num_widgets)
-		#create easy access to the R object
-		self.r = r
-		
-	#convert R data.frames to Orange exampleTables
-	def convertDataframeToExampleTable(self, dataFrame_name):
-		set_default_mode(CLASS_CONVERSION)
-		dataFrame = self.r(dataFrame_name)	
-		col_names = self.r('colnames(' + dataFrame_name + ')')
-		col_def = self.r.sapply(dataFrame,'class')
-		colClasses = []
-		for i in col_names:
-			if col_def[i] == 'numeric' or col_def[i] == 'integer':
-				colClasses.append(orange.FloatVariable(i))
-			elif col_def[i] == 'factor':
-				colClasses.append(orange.StringVariable(i))
-			elif col_def[i] == 'character':
-				colClasses.append(orange.StringVariable(i))
+    #a class variable which is incremented every time OWRpy is instantiated.
+    
+    num_widgets = 0
+    def __init__(self):	
+        #The class variable is used to create the unique names in R
+        OWRpy.num_widgets += 1
+        #this should be appended to every R variable
+        self.variable_suffix = '_' + str(OWRpy.num_widgets)
+        #create easy access to the R object
+        
+        
+    #convert R data.frames to Orange exampleTables
+    def convertDataframeToExampleTable(self, dataFrame_name):
+        set_default_mode(CLASS_CONVERSION)
+
+        dataFrame = r(dataFrame_name)	
+        col_names = r('colnames(' + dataFrame_name + ')')
+        col_def = r.sapply(dataFrame,'class')
+        colClasses = []
+        for i in col_names:
+            if col_def[i] == 'numeric' or col_def[i] == 'integer':
+                colClasses.append(orange.FloatVariable(i))
+            elif col_def[i] == 'factor':
+                colClasses.append(orange.StringVariable(i))
+            elif col_def[i] == 'character':
+                colClasses.append(orange.StringVariable(i))
 
 
+        r('exampleTable_data' + self.variable_suffix + '<- '+ dataFrame_name)
+        r('exampleTable_data' + self.variable_suffix + '[is.na(' + dataFrame_name + ')] <- "?"')
 
-		d = r('as.matrix(' + dataFrame_name + ')')
-		domain = orange.Domain(colClasses)
-		data = orange.ExampleTable(domain, d)		
-		return data
-		
+        d = r('as.matrix(exampleTable_data' + self.variable_suffix + ')')
+        domain = orange.Domain(colClasses)
+        data = orange.ExampleTable(domain, d)
+        r('rm(exampleTable_data' + self.variable_suffix + ')')
+        return data
+        
--- OrangeWidgets/R/readFile.py
+++ OrangeWidgets/R/readFile.py
@@ -10,33 +10,43 @@ from OWWidget import *
 import OWGUI
 
 class readFile(OWWidget,OWRpy):
-	
-	def __init__(self, parent=None, signalManager=None):
-		OWWidget.__init__(self, parent, signalManager, "File", wantMainArea = 0, resizingEnabled = 1)
-		OWRpy.__init__(self)
-		self.inputs = None
-		self.outputs = [("data.frame", orange.Variable)]
-		box = OWGUI.widgetBox(self.controlArea, "Data File", addSpace = True, orientation=0)
-		self.filecombo = QComboBox(box)
-		self.filecombo.setMinimumWidth(150)
-		box.layout().addWidget(self.filecombo)
-		button = OWGUI.button(box, self, '...', callback = self.browseFile, width = 25, disabled=0)
-		box = OWGUI.widgetBox(self.controlArea, "Info", addSpace = True)
-		self.infoa = OWGUI.widgetLabel(box, 'No data loaded.')
-		self.infob = OWGUI.widgetLabel(box, 'file_suffix: ' + self.variable_suffix)
-		self.warnings = OWGUI.widgetLabel(box, ' ')
-	
-	
-	def browseFile(self): #should open a dialog to choose a file that will be parsed to set the wd
-		#something to handle the conversion
-		self.r('filename' + self.variable_suffix + ' <- choose.files()')
-		#self.infob.setText(self.r['filename' + self.variable_suffix])
-		if self.r.length(self.r['filename' + self.variable_suffix])[0] == 0:
-			return
-		
+    
+    def __init__(self, parent=None, signalManager=None):
+        OWWidget.__init__(self, parent, signalManager, "File", wantMainArea = 0, resizingEnabled = 1)
+        OWRpy.__init__(self)
+        self.inputs = None
+        
+        self.outputs = [("data.frame", orange.Variable),("Examples", ExampleTable)]
+        box = OWGUI.widgetBox(self.controlArea, "Data File", addSpace = True, orientation=0)
+        self.filecombo = QComboBox(box)
+        self.filecombo.setMinimumWidth(150)
+        box.layout().addWidget(self.filecombo)
+        button = OWGUI.button(box, self, '...', callback = self.browseFile, width = 25, disabled=0)
+        box = OWGUI.widgetBox(self.controlArea, "Info", addSpace = True)
+        self.infoa = OWGUI.widgetLabel(box, 'No data loaded.')
+        self.infob = OWGUI.widgetLabel(box, '')
+        self.infoc = OWGUI.widgetLabel(box, '')
+        self.infod = OWGUI.widgetLabel(box, '')
+        
+    
+    
+    def browseFile(self): #should open a dialog to choose a file that will be parsed to set the wd
+        #something to handle the conversion
+        filename = 'filename' + self.variable_suffix
+        dataframe = 'data' + self.variable_suffix
+        fn = r(filename + ' <- choose.files()')
+        if r('length(' + filename +')') == 0:
+            return
+        
+        
+        data = r(dataframe + '= read.delim(' + filename + ')')
+        col_names = r('colnames(' + dataframe + ')')
+        self.infoa.setText("data loaded")
+        self.infob.setText(fn)
+        self.infoc.setText("Number of rows: " + str(r('nrow(data' + self.variable_suffix + ')')))
+        col_def = r.sapply(data,'class')
+        self.infod.setText(str(col_def))
+        
 
-		data = r('data' + self.variable_suffix + '= read.delim(filename' + self.variable_suffix + ')')
-		r('exampleTable_data' + self.variable_suffix + '<- data'+self.variable_suffix)
-		r('exampleTable_data' + self.variable_suffix + '[is.na(data' + self.variable_suffix + ')] <- "?"')
-		self.send("Examples", self.convertDataframeToExampleTable('exampleTable_data' + self.variable_suffix))
-		self.send("data.frame", 'data' + self.variable_suffix)>>>>>>> .r23
+        self.send("data.frame", 'data' + self.variable_suffix)
+        self.send("Examples", self.convertDataframeToExampleTable(dataframe))
