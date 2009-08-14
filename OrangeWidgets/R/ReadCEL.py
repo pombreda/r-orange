52580cee6704009f1aab79598998a5a36aeef301
covingto@bcm.edu <covingto@bcm.edu@1447cd30-8082-11de-88cc-eb9610995869>
2009-08-14 17:12:40 +0000

    Made changes to reflect the save functionality of orange and the updated naming conventions.

--- OrangeWidgets/R/ReadCEL.py
+++ OrangeWidgets/R/ReadCEL.py
@@ -5,9 +5,6 @@
 <priority>10</priority>
 """
 
-from rpy_options import set_options
-set_options(RHOME='c:/progra~1/r/R-2.6.2/')
-from rpy import *
 from OWWidget import *
 import OWGUI
 import orngIO
@@ -16,25 +13,32 @@ from OWRpy import *
 r.require('affy')
 
 class ReadCEL(OWWidget, OWRpy):
+	settingsList = ['vs', 'FD', 'eset', 'folder']
 	def __init__(self, parent=None, signalManager=None):
 		OWWidget.__init__(self, parent, signalManager, "File", wantMainArea = 0, resizingEnabled = 1)
 		OWRpy.__init__(self)
 		
+		self.eset = {}
+		self.vs = self.variable_suffix
+		self.FD = 'choose.dir()'
+		self.loadSettings()
+		
 		self.inputs = None #There is the posibility of inputs to set the class, phenotype, abstract, etc. of the affybatch
 		self.outputs = [("Affybatch Expression Matrix", orange.Variable)]
 		
-		self.state = {}
-		self.celfilepath = None #sets the celfilepath to the current wd, not that there should be anything in there that it can read but it's better than nothing
-		self.state['vs'] = self.variable_suffix
-		self.state['FD'] = 'choose.dir()'
+		if 'eset' in self.eset: #check if data was loaded from a previous session and resend the data if so.
+			self.send("Affybatch Expression Matrix", self.eset)
 		
+		self.Rab = 'affybatch_'+self.vs	
+		self.FN = 'fnt'+self.vs
+		self.folder = ''
 		#Build a GUI to select either a folder or a txt file with the names of file locations in it to pass to the ReadAffy command of bioconductor
 		#This should include a button for processing the files and a window for viewing the contents of the txt file (possibly added in the future)
 		
 		#folderselect box
-		#folderSelect = OWGUI.widgetBox(self.controlArea, "Select a Folder")
-		#OWGUI.lineEdit(folderSelect, self, "folder", "Selected Folder:", labelWidth=70, orientation="horizontal")
-		#button = OWGUI.button(folderSelect, self, 'Choose File', callback = self.browseFile, width = 100, disabled=0)
+		folderSelect = OWGUI.widgetBox(self.controlArea, "Select a Folder")
+		OWGUI.lineEdit(folderSelect, self, "folder", "Selected Folder:", labelWidth=70, orientation="horizontal")
+		button = OWGUI.button(folderSelect, self, 'Choose File', callback = self.browseFile, width = 100, disabled=0)
 		
 		#the process box
 		processbox = OWGUI.widgetBox(self.controlArea, "Process")
@@ -43,22 +47,22 @@ class ReadCEL(OWWidget, OWRpy):
 		self.infob = OWGUI.widgetLabel(processbox, 'file_suffix: ' + self.variable_suffix)
 	
 	def browseFile(self): #should open a dialog to choose a file that will be parsed to set the wd
-		FN = 'r.choose.dir()'
-		#something to handle the conversion
-		self.celfilepath = FN
-		
-	def openFolder(self, FN):#this should send the folder name to the ReadAffy folder to use for the ReadAFFy command
-		self.celfilepath = FN[0]
+		r(self.FN+'<-choose.dir()')
+		self.folder = str(r(self.FN))
 		
 	def procesS(self):
-		#QCursor.setShape(QCursor.shape(3))
-		if self.celfilepath == None:
-			r('affybatch_'+self.state['vs']+'<-ReadAffy(celfile.path='+self.state['FD']+')')
-			self.state['eset'] = {'data':['exprs(affybatch_'+self.state['vs']+')'], 'eset':['affybatch_'+self.state['vs']]}
-			self.infoa.setText("Your data has been processed.")
-		else:
-			r('affybatch_'+self.state['vs']+'<-ReadAffy(celfile.path='+self.state['FD']+')')
-			self.state['eset'] = {'data':['exprs(affybatch_'+self.state['vs']+')'], 'eset':['affybatch_'+self.state['vs']]}
 		
-		self.send("Affybatch Expression Matrix", self.state['eset'])
-		#QCursor.setShape(QCursor.shape(0))
+		# block to set the celfile FN
+		try:
+			r(self.FN) #should throw and error if self.FN DNE
+		except:
+			self.browseFiles()
+			self.procesS()
+		
+		self.infoa.setText("Your data is processing")
+		
+		r(self.Rab+'<-ReadAffy(celfile.path='+self.FN+')')
+		self.eset = {'data':['exprs('+self.Rab+')'], 'eset':[self.Rab]}
+		self.infoa.setText("Your data has been processed.")
+	
+		self.send("Affybatch Expression Matrix", self.eset)
--- OrangeWidgets/R/panpCalls.py
+++ OrangeWidgets/R/panpCalls.py
@@ -4,10 +4,6 @@
 <icon>icons/readcel.png</icons>
 <priority>2010</priority>
 """
-
-from rpy_options import set_options
-set_options(RHOME='c:/progra~1/r/R-2.6.2/')
-from rpy import *
 from OWWidget import *
 from OWRpy import *
 import OWGUI
@@ -17,17 +13,25 @@ r.require('limma')
 r.require('panp')
 
 class panpCalls(OWWidget, OWRpy):
+	settingsList = ['vs', 'senddata', 'looseCut', 'tightCut', 'percentA', 'data', 'eset']
 	def __init__(self, parent=None, signalManager=None):
 		OWWidget.__init__(self, parent, signalManager, "Sample Data")
 		OWRpy.__init__(self)
-		self.inputs = [("Expression Set", orange.Variable, self.process)]
-		self.outputs = [("Present Gene Signal Matrix", orange.Variable)]
 		
-		self.state = {}
-		self.state['vs'] = self.variable_suffix
+		self.senddata = {}
+		self.data = {}
+		self.eset = ''
+		self.vs = self.variable_suffix
 		self.looseCut = '0.02'
 		self.tightCut = '0.01'
 		self.percentA = '20'
+		self.peset = 'peset'+str(self.vs)
+		self.loadSettings()
+		
+		self.inputs = [("Expression Set", orange.Variable, self.process)]
+		self.outputs = [("Present Gene Signal Matrix", orange.Variable)]
+		
+		
 		#GUI
 		box = OWGUI.widgetBox(self.controlArea, "Options")
 		
@@ -40,9 +44,9 @@ class panpCalls(OWWidget, OWRpy):
 		
 	def process(self, dataset):
 		if dataset:
-			self.state['data'] = dataset
-			if 'eset' in self.state['data']:
-				self.eset = self.state['data']['eset'][0]
+			self.data = dataset
+			if 'eset' in self.data:
+				self.eset = self.data['eset'][0]
 			else:
 				self.infoa.setText("Processing imposible, not of eset or affybatch type")
 		else:
@@ -55,14 +59,11 @@ class panpCalls(OWWidget, OWRpy):
 		r('PAcalls<-PA$Pcalls == "A"')
 		r('PAcalls_sum<-apply(PAcalls, 1, sum)')
 		r('Present<- PAcalls_sum/length(PAcalls[1,]) > '+self.percentA+'/100')
-		r('peset_'+str(self.state['vs'])+'<-exprs('+self.eset+')[Present,]')
+		r(self.peset+'<-exprs('+self.eset+')[Present,]')
 		self.infoa.setText('Expression matrix with expression values for present genes has been created and sent!  You may now generate a fit using the Diff Exp widget')
-		emat = {'data':['peset_'+str(self.state['vs'])]}
-		self.send('Present Gene Signal Matrix', emat)
-		self.setState()
-		
-	def setState(self):
-		self.state['looseCut'] = self.looseCut
-		self.state['tightCut'] = self.tightCut
-		self.state['percentA'] = self.percentA
+		self.senddata = self.data.copy()
+		self.senddata['data'] = [self.peset]
+		self.send('Present Gene Signal Matrix', self.senddata)
+
+
 		
