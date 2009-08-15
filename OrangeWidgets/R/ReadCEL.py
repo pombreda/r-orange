495d94791ac233ac4c3b117435b0191c7303c8bc
covingto@bcm.edu <covingto@bcm.edu@1447cd30-8082-11de-88cc-eb9610995869>
2009-08-15 17:22:05 +0000

    changes to readcel

--- OrangeWidgets/R/ReadCEL.py
+++ OrangeWidgets/R/ReadCEL.py
@@ -11,88 +11,57 @@ import orngIO
 from OWRpy import *
 
 
-
 class ReadCEL(OWWidget, OWRpy):
-    settingsList = ['variable_suffix','recentFiles']
-    def __init__(self, parent=None, signalManager=None):
-        OWWidget.__init__(self, parent, signalManager, "File", wantMainArea = 0, resizingEnabled = 1)
-        OWRpy.__init__(self)
-
-        #default GUI values        
-        self.recentFiles = ['(none)']
-        self.loadSettings()
-        
-        #required librarys
-        self.require_librarys(['affy'])
-        
-        
-        #set R variable names
-        self.Rvariables = self.setRvariableNames(['eset','folder'])
-        
-        #signals
-        self.inputs = None 
-        self.outputs = [("Affybatch Expression Matrix", orange.Variable)]
-
-
-        #GUI
-        box = OWGUI.widgetBox(self.controlArea, "Select Folder", addSpace = True, orientation=0)
-        self.filecombo = QComboBox(box)
-        self.filecombo.setMinimumWidth(150)
-        box.layout().addWidget(self.filecombo)
-        button = OWGUI.button(box, self, '...', callback = self.browseFile, width = 25, disabled=0)
-        box = OWGUI.widgetBox(self.controlArea, "Info", addSpace = True)
-        self.infoa = OWGUI.widgetLabel(box, 'No data loaded.')
-        self.infob = OWGUI.widgetLabel(box, '')
-        self.infoc = OWGUI.widgetLabel(box, '')
-        self.infod = OWGUI.widgetLabel(box, '')
-        self.setFileList()
-        self.connect(self.filecombo, SIGNAL('activated(int)'), self.selectFile)
-
-        
-        #initialize previous sessions
-        if self.rsession('exists("' + self.Rvariables['eset'] + '")'):
-            self.procesS(False)
-
-    def setFileList(self):
-        self.filecombo.clear()
-        if not self.recentFiles:
-            self.filecombo.addItem("(none)")
-        for file in self.recentFiles:
-            if file == "(none)":
-                self.filecombo.addItem("(none)")
-            else:
-                self.filecombo.addItem(file)
-        
-
-    def selectFile(self, n):
-        if n < len(self.recentFiles) :
-            name = self.recentFiles[n]
-            self.recentFiles.remove(name)
-            self.recentFiles.insert(0, name)
-        elif n:
-            self.browseFile(1)
-
-        if len(self.recentFiles) > 0:
-            self.setFileList()
-        self.rsession(self.Rvariables['folder'] + ' = "' + self.recentFiles[0].replace('\\', '\\\\') + '"')
-        self.procesS(True)
-        
-        
-    def browseFile(self): #should open a dialog to choose a file that will be parsed to set the wd
-        folder = self.rsession(self.Rvariables['folder'] +'<-choose.dir()')
-        if self.rsession('length(' + self.Rvariables['folder'] +')') != 0:
-            if folder in self.recentFiles: self.recentFiles.remove(folder)
-            self.recentFiles.insert(0, folder)
-            self.setFileList()
-            self.procesS(True)
-        
-    def procesS(self,load):
-        
-        self.infoa.setText("Your data is processing")
-        if load:
-            self.rsession(self.Rvariables['eset']+'<-ReadAffy(celfile.path='+self.Rvariables['folder']+')')
-        
-        self.out = {'data':'exprs('+self.Rvariables['eset']+')', 'eset':self.Rvariables['eset']}
-        self.infoa.setText("Your data has been processed.")
-    
-        self.send("Affybatch Expression Matrix", self.out)
+	settingsList = ['vs', 'FD', 'eset', 'folder']
+	def __init__(self, parent=None, signalManager=None):
+		OWWidget.__init__(self, parent, signalManager, "File", wantMainArea = 0, resizingEnabled = 1)
+		OWRpy.__init__(self)
+		
+		self.eset = {}
+		self.vs = self.variable_suffix
+		self.FD = 'choose.dir()'
+		self.loadSettings()
+		self.rsession('require("affy")')
+		self.inputs = None #There is the posibility of inputs to set the class, phenotype, abstract, etc. of the affybatch
+		self.outputs = [("Affybatch Expression Matrix", orange.Variable)]
+		
+		if 'eset' in self.eset: #check if data was loaded from a previous session and resend the data if so.
+			self.send("Affybatch Expression Matrix", self.eset)
+		
+		self.Rab = 'affybatch_'+self.vs	
+		self.FN = 'fnt'+self.vs
+		self.folder = ''
+		#Build a GUI to select either a folder or a txt file with the names of file locations in it to pass to the ReadAffy command of bioconductor
+		#This should include a button for processing the files and a window for viewing the contents of the txt file (possibly added in the future)
+		
+		#folderselect box
+		folderSelect = OWGUI.widgetBox(self.controlArea, "Select a Folder")
+		OWGUI.lineEdit(folderSelect, self, "folder", "Selected Folder:", labelWidth=70, orientation="horizontal")
+		button = OWGUI.button(folderSelect, self, 'Choose File', callback = self.browseFile, width = 100, disabled=0)
+		
+		#the process box
+		processbox = OWGUI.widgetBox(self.controlArea, "Process")
+		processbutton = OWGUI.button(processbox, self, 'Process', callback = self.procesS, width = 200)
+		self.infoa = OWGUI.widgetLabel(processbox, 'Data not yet processed.')
+		self.infob = OWGUI.widgetLabel(processbox, 'file_suffix: ' + self.variable_suffix)
+	
+	def browseFile(self): #should open a dialog to choose a file that will be parsed to set the wd
+		self.rsession(self.FN+'<-choose.dir()')
+		self.folder = str(self.rsession(self.FN))
+		
+	def procesS(self):
+		
+		# block to set the celfile FN
+		try:
+			self.rsession(self.FN) #should throw and error if self.FN DNE
+		except:
+			self.browseFiles()
+			self.procesS()
+		
+		self.infoa.setText("Your data is processing")
+		
+		self.rsession(self.Rab+'<-ReadAffy(celfile.path='+self.FN+')')
+		self.eset = {'data':'exprs('+self.Rab+')', 'eset':self.Rab}
+		self.infoa.setText("Your data has been processed.")
+	
+		self.send("Affybatch Expression Matrix", self.eset)
