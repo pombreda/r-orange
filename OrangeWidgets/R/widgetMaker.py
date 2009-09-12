83e9838bf9ea6a15c9c312d523cff88c9acadbb2
covingto@bcm.edu <covingto@bcm.edu@1447cd30-8082-11de-88cc-eb9610995869>
2009-09-12 19:45:52 +0000

    Addition of the widgetMaker widget and some other changes

---
+++ OrangeWidgets/R/widgetMaker.py
@@ -0,0 +1,232 @@
+"""
+<name>Widget Maker</name>
+<description>A widget for making the initial framework of a functional widget given an R package and function.</description>
+<icon>icons/widgetMaker.png</icon>
+<author>Kyle R. Covington</author>
+"""
+
+from OWRpy import *
+import OWGUI
+
+class widgetMaker(OWRpy):
+    def __init__(self, parent=None, signalManager=None):
+        settingsList = ['output_txt', 'parameters']
+        OWRpy.__init__(self, parent, signalManager, "File", wantMainArea = 1, resizingEnabled = 1)
+        
+        self.packageName = ''
+        self.functionParams = ''
+        self.functionName = ''
+        self.widgetInputsName = []
+        self.widgetInputsClass = []
+        self.widgetInputsFunction = []
+        self.numberInputs = 0
+        self.numberOutputs = 0
+        
+        self.fieldList = {}
+        self.functionInputs = {}
+        self.functionAllowOutput = 1
+        self.processOnConnect = 1
+        
+        self.inputs = None
+        self.outputs = None
+        
+        # GUI
+        # several tabs with different parameters such as loading in a function, setting parameters, setting inputs and outputs
+        
+        box = OWGUI.widgetBox(self.controlArea, "")
+        self.infoa = OWGUI.widgetLabel(box, '')
+        OWGUI.lineEdit(box, self, 'packageName', label = 'Package:', orientation = 1)
+        OWGUI.button(box, self, 'Load Package', callback = self.loadRPackage)
+        OWGUI.lineEdit(box, self, 'functionName', label = 'Function Name:', orientation = 1)
+        OWGUI.button(box, self, 'Parse Function', callback = self.parseFunction)
+        
+        box = OWGUI.widgetBox(self.controlArea, "Inputs and Outputs")
+        self.inputArea = QTableWidget()
+        box.layout().addWidget(self.inputArea)
+        self.inputArea.setColumnCount(3)
+        
+        self.inputArea.hide()
+        self.connect(self.inputArea, SIGNAL("itemClicked(QTableWidgetItem*)"), self.inputcellClicked)
+        OWGUI.button(box, self, 'Accept Inputs', callback = self.acceptInputs)
+        OWGUI.checkBox(box, self, 'functionAllowOutput', 'Allow Output')
+        
+        OWGUI.button(box, self, 'Generate Code', callback = self.generateCode)
+        
+        self.splitCanvas = QSplitter(Qt.Vertical, self.mainArea)
+        self.mainArea.layout().addWidget(self.splitCanvas)
+        
+        codebox = OWGUI.widgetBox(self, "Code Box")
+        self.splitCanvas.addWidget(codebox)
+
+        self.codeArea = QTextEdit()
+        codebox.layout().addWidget(self.codeArea)
+        
+
+        
+    def loadRPackage(self):
+        
+        self.require_librarys([self.packageName])
+        
+    def parseFunction(self):
+        self.args = {}
+        try:
+            holder = self.R('capture.output(args('+self.functionName+'))')
+            s = ''
+            functionParams = s.join(holder)
+            print self.functionName
+            self.infoa.setText("Function called successfully.")
+            print 'function called successfully'
+            print str(self.functionParams)
+        except:
+            self.infoa.setText("Error with calling function.")
+            return
+            
+        start = functionParams.find('(')+1 #where the args start 
+        print str(start)
+        end = functionParams.rfind(')') # where the args end.
+        print 'end'+str(end)
+        tmp = functionParams[start:end]
+        
+        tmp = tmp.replace(' ','') #remove the spaces
+        tmp = tmp.replace("','", '##')
+        tmp = tmp.replace('","', '##')
+        tmp = tmp.split(',')
+        for el in tmp:
+            tmp2 = el.split('=')
+            if len(tmp2) > 1:
+                self.args[tmp2[0]] = tmp2[1]
+            else:
+                self.args[tmp2[0]] = ''
+            
+        for arg in self.args.keys():
+            if self.args[arg][0:2] == 'c(':
+                self.args[arg] = self.args[arg].replace("'", '')
+                self.args[arg] = self.args[arg].replace('"', '')
+                start = self.args[arg].find('(')+1
+                end = self.args[arg].rfind(')')
+                self.args[arg] = self.args[arg][start:end] #strip out the brackets
+                self.args[arg] = self.args[arg].replace('##', ",")
+                self.args[arg] = self.args[arg].split(',')
+                
+        self.updateInputs()
+        
+    def updateInputs(self):
+        self.inputArea.clear()
+        self.inputArea.setRowCount(int(len(self.args.keys())))
+
+        self.inputArea.show()
+        self.inputArea.setHorizontalHeaderLabels(['Name', 'Class', 'Function'])
+        n=0
+        for arg in self.args.keys():
+            itemname = QTableWidgetItem(str(arg))
+            #itemClass = QTableWidgetItem
+            self.inputArea.setItem(n,0,itemname)
+            cw = QComboBox()
+            cw.addItems(['Widget Input', 'Connection Input'])
+            self.inputArea.setCellWidget(n,1,cw)
+            n += 1
+        
+    def acceptInputs(self): #accept the criteria in the input Area
+        for i in xrange(self.inputArea.rowCount()):
+            print 'i:'+str(i)
+            combo = self.inputArea.cellWidget(i, 1)
+            print combo
+            if combo.currentText() == 'Widget Input':
+                print str(self.inputArea.item(i ,0).text())
+                self.fieldList[str(self.inputArea.item(i, 0).text())] = ''
+            else:
+                self.functionInputs[str(self.inputArea.item(i,0).text())] = ''
+        print self.fieldList
+        print self.functionInputs
+        for item in self.fieldList.keys():
+            self.fieldList[item] = self.args[item]
+            
+    def inputcellClicked(self, item):
+        self.inputArea.editItem(item)
+    
+    def generateCode(self):
+        self.makeHeader()
+        self.makeInitHeader()
+        self.makeGUI()
+        self.makeProcessSignals()
+        self.makeCommitFunction()
+        #self.makeRsendFunction()
+        
+        self.combineCode()
+        
+    def makeHeader(self):
+        self.headerCode = '"""\n'
+        self.headerCode += '&lt;name&gt;'+self.functionName+'&lt;/name&gt;\n'
+        self.headerCode += '&lt;author&gt;Generated using Widget Maker written by Kyle R. Covington&lt;/author&gt;\n'
+        self.headerCode += '"""\n'
+        self.headerCode += 'from OWRpy import * \n'
+        self.headerCode += 'import OWGUI \n'
+        
+    def makeInitHeader(self):
+        self.initCode = ''
+        self.initCode += 'class '+self.functionName+'(OWRpy): \n'
+        self.initCode += '\tdef __init__(self, parent=None, signalManager=None):\n'
+        self.initCode += '\t\tsettingsList = []\n'
+        self.initCode += '\t\tOWRpy.__init__(self, parent, signalManager, "File", wantMainArea = 0, resizingEnabled = 1)\n'
+        for element in self.fieldList.keys():
+            if element == '...':
+                pass
+            else:
+                if self.fieldList[element] == '' or self.fieldList[element] == "":
+                    self.initCode += '\t\tself.'+element+' = ""\n'
+                elif type(self.fieldList[element]) == type([]): #the fieldList is a list
+                    self.initCode += '\t\tself.'+element+' = 0\n' #set the item to the first one
+                else:
+                    self.initCode += '\t\tself.'+element+' = "'+str(self.fieldList[element])+'"\n'
+        if len(self.functionInputs.keys()) > 0:
+            self.initCode += '\t\tself.inputs = ['
+            for element in self.functionInputs.keys():
+                self.initCode += '("'+element+'", RvarClasses.RVariable, self.process'+element+'),'
+            self.initCode = self.initCode[:len(self.initCode)-1]
+            self.initCode += ']\n'
+        if self.functionAllowOutput:
+            self.initCode += '\t\tself.outputs = [("'+self.functionName+' Output", RvarClasses.RVariable)]\n'
+        self.initCode += '\t\t\n'
+        
+    def makeGUI(self):
+        self.guiCode = ''
+        self.guiCode += '\t\tbox = OWGUI.widgetBox(self.controlArea, "Widget Box")\n'
+        for element in self.fieldList.keys():
+            if element == '...':
+                pass
+            else:
+                if type(self.fieldList[element]) == type(''):
+                    self.guiCode += '\t\tOWGUI.lineEdit(box, self, "'+element+'", label = "'+element+':")\n'
+                elif type(self.fieldList[element]) == type([]):
+                    self.guiCode += '\t\tOWGUI.comboBox(box, self, "'+element+'", label = "'+element+':", items = '+str(self.fieldList[element])+')\n'
+        self.guiCode += '\t\tOWGUI.button(box, self, "Commit", callback = self.commitFunction)\n'
+
+    def makeProcessSignals(self):
+        self.processSignals = ''
+        for inputName in self.functionInputs.keys():
+            self.processSignals += '\tdef process'+inputName+'(self, data):\n'
+            self.processSignals += '\t\tif data:\n'
+            self.processSignals += '\t\t\tself.'+inputName+'=data["data"]\n'
+            if self.processOnConnect:
+                self.processSignals += '\t\t\tself.commitFunction()\n'
+                
+    def makeCommitFunction(self):
+        self.commitFunction = ''
+        self.commitFunction += '\tdef commitFunction(self):\n'
+        self.commitFunction += "\t\tself.R(self.Rvariables['"+self.functionName+"']+'&lt;-"+self.functionName+"("
+        for element in self.functionInputs.keys():
+            self.commitFunction += element+"='+str(self."+element+")+',"
+        for element in self.fieldList.keys():
+            if element == '...':
+                pass
+            else:
+                self.commitFunction += element+"='+str(self."+element+")+',"
+        self.commitFunction += ")')\n"
+        if self.functionAllowOutput:
+            self.commitFunction += '\t\tself.rSend("'+self.functionName+' Output", {"data":self.Rvariables["'+self.functionName+'"]})\n'
+    
+    def combineCode(self):
+        self.completeCode = '<pre>'
+        self.completeCode += self.headerCode+self.initCode+self.guiCode+self.processSignals+self.commitFunction
+        self.completeCode += '</pre>'
+        self.codeArea.setHtml(self.completeCode)
---
+++ OrangeWidgets/Survival/survivalTest.py
@@ -0,0 +1,63 @@
+"""
+<name>Survival Test</name>
+<description>Compare one or more sets of survival data.</description>
+<icon>icons/survival.png</icon>
+<priority>10</priority>
+"""
+# allows the user to calculate survival or hazard ratios for many kinds of data.  Assumes non-proportional hazards which can be adjusted on an 'advanced' tab if the user wishes.
+
+from OWRpy import *
+import OWGUI
+
+class survivalTest(OWRpy):
+    #This widget has no settings list
+    def __init__(self, parent=None, signalManager=None):
+        settingsList = ['output_txt', 'parameters']
+        OWRpy.__init__(self, parent, signalManager, "File", wantMainArea = 0, resizingEnabled = 1)
+        
+        self.output_txt = '' # the R output for the survival test
+        self.parameters = [] # start with a blank list
+        
+        
+        self.inputs = [("Data Table", RvarClasses.RDataFrame, self.processData)]
+        self.outputs = [("Survival Fit", RvarClasses.RVariable)]
+        
+        # Start the GUI
+        box = OWGUI.widgetBox(self.controlArea, "Options")
+        self.eventsList = OWGUI.comboBox(box, self, 'events', label = 'Events')
+        self.timeList = OWGUI.comboBox(box, self, 'time', lable = 'Time')
+        self.paramsList = OWGUI.listBox(box, self, 'parameters', label = "Parameters")
+        
+        
+        
+        output = OWGUI.widgetBox(self.controlArea, "Output")
+        self.ROutputViewer = QTextEdit()
+        output.layout().addWidget(self.ROutputViewer)
+        # End the GUI
+        
+    def onLoadSavedSession(self):
+        self.ROutputViewer.setHtml(self.output_txt)
+    
+    def processData(self, data):
+        self.clear()
+        if data != None: #must account for an empty dataset
+            self.data = data['data']
+            parameters = self.R('colnames('+self.data+')')
+            if self.parameters != parameters:
+                self.parameters = parameters # resets the GUI if new colnames are detected otherwise skip
+                self.refresh()
+            else:
+                self.commit()
+        else:   
+            self.ROutputViewer.setHtml("No data connected or sent.  Please check connection")
+    def commit(self):
+        # stuff for processing the data
+    def clear(self):
+        self.ROutputViewer.clear()
+        
+    def refresh(self):
+        self.eventsList.clear()
+        self.timeList.clear()
+        self.paramsList.clear()
+        self.eventsList.addItems(self.parameters)
+        self.timeList.addItems(self.parameters)
