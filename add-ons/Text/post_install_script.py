5a74e943c771265ac6ed44f528106b13e1486cfa
covingto@bcm.edu <covingto@bcm.edu@1447cd30-8082-11de-88cc-eb9610995869>
2009-09-14 14:28:36 +0000

    removal of these packages

--- add-ons/Text/post_install_script.py
+++
@@ -1,6 +0,0 @@
-import distutils
-import distutils.sysconfig
-import sys
-import orngEnviron
-
-#  no need to do anything
--- add-ons/myModule/_pyxtract.bat
+++
@@ -1,2 +0,0 @@
-python ../pyxtract/pyprops.py -l ../orange/ppp/stamp -n mymodule
-python ../pyxtract/pyxtract.py -m -n mymodule -l ../orange/px/stamp cls_myclasses.cpp
