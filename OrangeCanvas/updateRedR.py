829bfdc49c993831b40ac61a8805f6c75d1b4da6
covingto@bcm.edu <covingto@bcm.edu@1447cd30-8082-11de-88cc-eb9610995869>
2009-10-15 21:25:48 +0000

    finally got the versions right

--- OrangeCanvas/updateRedR.py
+++ OrangeCanvas/updateRedR.py
@@ -17,9 +17,13 @@ def start(lastRevproplist, versionNumber):
     
     #versionNumber = 'Version0'
     #versionNumber = 'Version1.5'
-
-    svnLoc = 'http://r-orange.googlecode.com/svn/branches/'
-
+    # movie = MoviePlayer()
+    # movie.show()
+    # movie.start()
+    # QMessageBox.information(None, 'RedR', str(versionNumber), QMessageBox.Ok)
+    # movie.stop()
+    # movie.hide()
+    svnLoc = 'http://r-orange.googlecode.com/svn/trunk/'
     
     try:
         client = pysvn.Client()
