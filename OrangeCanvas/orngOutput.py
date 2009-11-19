96be68b02f7c4d2bae76d4efc68d3859346f13eb
covingto@bcm.edu <covingto@bcm.edu@1447cd30-8082-11de-88cc-eb9610995869>
2009-11-19 05:18:16 +0000

    just changing the counter

--- OrangeCanvas/orngOutput.py
+++ OrangeCanvas/orngOutput.py
@@ -1,4 +1,4 @@
-# Author: Gregor Leban (gregor.leban@fri.uni-lj.si)
+# Author: Gregor Leban (gregor.leban@fri.uni-lj.si) modified by Kyle R Covington and Anup Parikh
 # Description:
 #     print system output and exceptions into a window. Enables copy/paste
 #
@@ -97,7 +97,7 @@ class OutputWindow(QDialog):
         # print text
         # return
         self.numberofLines += 1
-        if self.numberofLines > 1000:
+        if self.numberofLines > 100:
             self.textOutput.clear()
             self.numberofLines = 0
         Text = self.getSafeString(text)
@@ -134,7 +134,7 @@ class OutputWindow(QDialog):
         for line in lines:
             self.numberofLines += 1
             self.write(line)
-            if self.numberofLines > 1000:
+            if self.numberofLines > 100:
                 self.textOutput.clear()
 
     def flush(self):
