a6bd6b3c463d392b05a8120b0f302e86acd30a71
anup.parikh <anup.parikh@1447cd30-8082-11de-88cc-eb9610995869>
2010-03-07 17:59:02 +0000

    created singleClass dir which holds all the signal types

---
+++ OrangeWidgets/signalClasses/RvarClasses.py
@@ -0,0 +1,14 @@
+# Kyle R Covington
+class RVariable:
+    pass
+class RDataFrame(RVariable):
+    pass
+    
+class RVector(RVariable):
+    pass
+
+class RList(RVariable):
+    pass
+    
+class RSession(RVariable):
+    pass
