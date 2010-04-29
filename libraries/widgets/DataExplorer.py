91da692108140f8beb9ed6a68dd69f311f89a3c1
kylecovington1 <kylecovington1@1447cd30-8082-11de-88cc-eb9610995869>
2010-04-29 02:37:23 +0000

    fixes for DataExplorer

--- libraries/widgets/DataExplorer.py
+++ libraries/widgets/DataExplorer.py
@@ -389,7 +389,7 @@ class DataExplorer(OWRpy):
         if len(self.criteriaList) > 0:
             self.R(self.dataParent.dictAttrs['cm'][0]+'$'+self.Rvariables['dataExplorer']+'<-list(True = rownames('+self.dataParent.parent+'['+'&'.join(self.criteriaList)+',]), False = rownames('+self.dataParent.parent+'[!('+'&'.join(self.criteriaList)+'),]))')
             newData = self.dataParent.copy()
-            newData.data = self.dataParent.parent+'[rownames('+self.dataParent.parent+') %in% '+self.dataParent['cm'][0]+'$'+self.Rvariables['dataExplorer']+'$True,]'
+            newData.data = self.dataParent.data+'['+self.dataParent['cm'][0]+'$'+self.Rvariables['dataExplorer']+'$True,]'
             self.rSend('Data Subset', newData)
         else:
             self.rSend('Data Subset', self.dataParent.copy())
