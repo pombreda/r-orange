9df9a9bfb5b422337f175cc81ddf3fa7cdf1b58f
kylecovington1 <kylecovington1@1447cd30-8082-11de-88cc-eb9610995869>
2010-06-02 15:32:19 +0000

    fixes

--- canvas/redREnviron.py
+++ canvas/redREnviron.py
@@ -38,7 +38,7 @@ def __getDirectoryNames():
     widgetSettingsDir = os.path.join(settingsDir, "RedRWidgetSettings")
     downloadsDir = os.path.join(settingsDir, "downloads")
     
-    documentsDir = os.path.join(os.environ['HOME'], 'Documents', 'Red-R')
+    documentsDir = os.path.join(os.path.expanduser('~'), 'Documents', 'Red-R')
     if not os.path.isdir(documentsDir):
         os.makedirs(documentsDir)
     templatesDir = os.path.join(documentsDir, 'Templates')
