da90aef4483a39402881bbad69d748b4ccdcb98a
kylecovington1 <kylecovington1@1447cd30-8082-11de-88cc-eb9610995869>
2010-06-05 02:58:17 +0000

    removing personal packages from svn control

--- libraries/pamr/signalClasses/RPAMRCVFit.py
+++
@@ -1,8 +0,0 @@
-from signals.RModelFit import RModelFit
-
-class RPAMRCVFit(RModelFit):
-    def __init__(self, data, checkVal = True):
-        RModelFit.__init__(self, data = data, checkVal = False)
-        
-    def convertToClass(self, valClass):
-        return self
