69b25821de0397d4c777507cc36620f9095cee9d
kylecovington1 <kylecovington1@1447cd30-8082-11de-88cc-eb9610995869>
2010-06-06 22:44:09 +0000

    an amazing discovery about cPickle

--- libraries/base/widgets/SandBox.py
+++ libraries/base/widgets/SandBox.py
@@ -1,6 +1,6 @@
 """
 <name>SandBox</name>
-<description></description>
+<description>a playground for widgets</description>
 <tags>Prototypes</tags>
 <priority>9010</priority>
 """
@@ -19,8 +19,17 @@ class SandBox(OWRpy):
         self.outputs = None
         self.R('data <- data.frame(a=rnorm(100),b=rnorm(100))')
         data = {'a':[1,2],'b':[3,4]}
+        import cPickle
+        
         #self.table = Rtable(self.controlArea,Rdata = 'data')
         
         # self.table = table(self.controlArea,data = data)
         self.table2 = redRGUI.Rtable(self.controlArea,Rdata = 'data')        
         self.table2.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        b = cPickle.dumps(self.table2, 2)
+        print b
+        
+        c = cPickle.loads(b)
+        
+        print 'Class c', c.__class__
