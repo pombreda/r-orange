082054de54008f4fec95f37fe98491521da811f1
kylecovington1 <kylecovington1@1447cd30-8082-11de-88cc-eb9610995869>
2010-08-28 03:06:30 +0000

    fixes for a conflict

--- libraries/pls/widgets/RedRRMSEP.py
+++ libraries/pls/widgets/RedRRMSEP.py
@@ -25,27 +25,27 @@ class RedRRMSEP(OWRpy):
 
 		self.outputs.addOutput('id0', 'RMSEP Output', redRRModelFit)
 
-		
-		button(self.bottomAreaRight, "Commit", callback = self.commitFunction)
-		self.RoutputWindow = textEdit(self.controlArea, label = "R Output Window")
-	def processobject(self, data):
-		if not self.require_librarys(["pls"]):
-			self.status.setText('R Libraries Not Loaded.')
-			return
-		if data:
-			self.RFunctionParam_object=data.getData()
-			#self.data = data
-			self.commitFunction()
-		else:
-			self.RFunctionParam_object=''
-	def commitFunction(self):
-		if str(self.RFunctionParam_object) == '': return
-		
-		self.R(self.Rvariables['RMSEP']+'<-RMSEP(object='+str(self.RFunctionParam_object)+')')
-		self.R('txt<-capture.output('+self.Rvariables['RMSEP']+')')
-		self.RoutputWindow.clear()
-		tmp = self.R('paste(txt, collapse ="\n")')
-		self.RoutputWindow.insertHtml('<br><pre>'+tmp+'</pre>')
-		newData = signals.redRRModelFit(data = self.Rvariables["RMSEP"]) # moment of variable creation, no preexisting data set.  To pass forward the data that was received in the input uncomment the next line.
-		#newData.copyAllOptinoalData(self.data)  ## note, if you plan to uncomment this please uncomment the call to set self.data in the process statemtn of the data whose attributes you plan to send forward.
-		self.rSend("id0", newData)
+        
+        button(self.bottomAreaRight, "Commit", callback = self.commitFunction)
+        self.RoutputWindow = textEdit(self.controlArea, label = "R Output Window")
+    def processobject(self, data):
+        if not self.require_librarys(["pls"]):
+            self.status.setText('R Libraries Not Loaded.')
+            return
+        if data:
+            self.RFunctionParam_object=data.getData()
+            #self.data = data
+            self.commitFunction()
+        else:
+            self.RFunctionParam_object=''
+    def commitFunction(self):
+        if str(self.RFunctionParam_object) == '': return
+        
+        self.R(self.Rvariables['RMSEP']+'<-RMSEP(object='+str(self.RFunctionParam_object)+')')
+        self.R('txt<-capture.output('+self.Rvariables['RMSEP']+')')
+        self.RoutputWindow.clear()
+        tmp = self.R('paste(txt, collapse ="\n")')
+        self.RoutputWindow.insertHtml('<br><pre>'+tmp+'</pre>')
+        newData = signals.redRRModelFit(data = self.Rvariables["RMSEP"]) # moment of variable creation, no preexisting data set.  To pass forward the data that was received in the input uncomment the next line.
+        #newData.copyAllOptinoalData(self.data)  ## note, if you plan to uncomment this please uncomment the call to set self.data in the process statemtn of the data whose attributes you plan to send forward.
+        self.rSend("id0", newData)
