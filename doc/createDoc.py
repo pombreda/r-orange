be779103572846b9414057bcb98a716726d94621
anup.parikh <anup.parikh@1447cd30-8082-11de-88cc-eb9610995869>
2011-05-09 00:02:32 +0000

    create module hierarchy images

--- doc/createDoc.py
+++ doc/createDoc.py
@@ -1,3 +1,5 @@
+graphCmd = 'python dep.py %s | dot -T png -o %s.png'
+
 import sys,os,glob,subprocess
 
 redRRoot = sys.argv[1]
@@ -15,17 +17,27 @@ for n in coreFiles:
     
     output = """%s
 =================================
-   
+
 .. automodule:: %s
    :members:
    :undoc-members:
    :show-inheritance:
-""" % (name,name)
+   
+.. image:: %s.png
+""" % (name,name,name)
     
     f = open(os.path.join(docRoot,'core',name+'.rst'),'w')
     f.write(output)
     f.close()
+    
+    cmd = graphCmd % (n,os.path.join(docRoot,'core',name))
+    os.system(cmd)
 
+    
+#########################################################################
+
+
+    
 packages = glob.glob(os.path.join(redRRoot,'libraries','*','package.xml'))
 for p in packages:
     package = os.path.split(os.path.split(p)[0])[1]
@@ -85,6 +97,9 @@ Signal Classes:
         f = open(os.path.join(docRoot,'libraries',package,'widgets',name+'.rst'),'w')
         f.write(output)
         f.close()
+        
+        cmd = graphCmd % (n,os.path.join(docRoot,'libraries',package,'widgets',name))
+        os.system(cmd)
 ###############################################
     qtwidgets = glob.glob(os.path.join(redRRoot,'libraries',package,'qtWidgets','*.py'))
 
@@ -106,7 +121,8 @@ Signal Classes:
         f = open(os.path.join(docRoot,'libraries',package,'qtWidgets',name+'.rst'),'w')
         f.write(output)
         f.close()
-
+        cmd = graphCmd % (n,os.path.join(docRoot,'libraries',package,'qtWidgets',name))
+        os.system(cmd)
 ###############################################
     signalClasses = glob.glob(os.path.join(redRRoot,'libraries',package,'signalClasses','*.py'))
 
@@ -127,8 +143,12 @@ Signal Classes:
         f = open(os.path.join(docRoot,'libraries',package,'signalClasses',name+'.rst'),'w')
         f.write(output)
         f.close()
-
         
+        cmd = graphCmd % (n,os.path.join(docRoot,'libraries',package,'signalClasses',name))
+        os.system(cmd)
+
+##################################################        
+
 cmd = os.path.join(os.path.abspath(docRoot),'make.bat') + ' html'
 print 'Running doc compiler: ' + cmd
 p = subprocess.Popen(cmd,stdout=subprocess.PIPE).communicate()[0]
